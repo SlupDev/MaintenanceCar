b57f53f105d13fdd78c4d51ac2df41fc9da3f985
Jérémy Hoarau <jeremyhoarau44@gmail.com>
2025-12-15 22:28:50 +0400

    Added new routes

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -7,6 +7,10 @@
 |
 */
 
+import BrandsController from '#controllers/brands_controller'
+import FilesController from '#controllers/files_controller'
+import MaintenancesController from '#controllers/maintenances_controller'
+import ModelsController from '#controllers/models_controller'
 import VehiclesController from '#controllers/vehicles_controller'
 import router from '@adonisjs/core/services/router'
 
@@ -17,3 +21,7 @@ router.get('/', async () => {
 })
 
 router.resource('vehicle', VehiclesController).apiOnly()
+router.resource('maintenance', MaintenancesController).apiOnly()
+router.resource('file', FilesController).apiOnly()
+router.resource('brand', BrandsController).apiOnly()
+router.resource('model', ModelsController).apiOnly()
